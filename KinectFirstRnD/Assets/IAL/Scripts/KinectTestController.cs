64d04c22dd1b45e0965dec754d13076f334e49a0
Aftab_Pi_Lover <aftab.pilover@gmail.com>
2019-04-15 20:38:32 +0600

    Second commit from Local Repo

---
+++ KinectFirstRnD/Assets/IAL/Scripts/KinectTestController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KinectTestController : MonoBehaviour
+{
+    [SerializeField] private Text kinectTestText;
+    KinectWrapper.NuiSkeletonData kinectNuiSkalatonData;
+    KinectManager myKinectManager;
+    private void Start()
+    {
+        myKinectManager = new KinectManager();
+    }
+
+    private void Update()
+    { 
+
+    }
+}
